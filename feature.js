6902c9f572c7e5405e3f46e11669c90e714d84c4
Ashrafii381684 <aliashrafmd19@gmail.com>
2024-12-08 18:57:45 +0600

    feature added

---
+++ feature.js
@@ -0,0 +1,6 @@
+document.getElementById('donation-button').addEventListener('click', function(){
+        showSectionById('donation-section');
+})
+document.getElementById('history-button').addEventListener('click', function(){
+        showSectionById('history-section');
+})
